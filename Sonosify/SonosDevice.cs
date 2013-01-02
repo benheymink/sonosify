b8e55aebd7929905c89f58db884bafd0faae7f00
benheymink <benheymink@gmail.com>
2013-01-02 09:16:45 +0000

    Added SonosDevice.cs class

---
+++ Sonosify/SonosDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sonosify
+{
+    class SonosDevice
+    {
+        public string Location { get; set; }
+        public string Name { get; set; }
+    }
+}
